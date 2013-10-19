41cb781704ebf41dfe8a1ed69d4cf1a74cef552d
HAUSCHKA <s1010345@pu.edu.tw>
2013-10-19 15:26:54 +0800

    FIX #8

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
 var gameModule = (function() {
+
+var timeoutVar,
+    counter = 0;
+
 function start() {
- var canvas = document.getElementById('game');
- var ctx = canvas.getContext('2d');
-var ballX = Math.floor(Math.random() * 300); // 0..300
-var ballY = Math.floor(Math.random() * 500); 
-var ballR = Math.floor(Math.random() * 100);
+     var canvas = document.getElementById('game'),
+ ctx = canvas.getContext('2d'),
+ ballX = Math.floor(Math.random() * 300), // 0..300
+ ballY = Math.floor(Math.random() * 500), 
+ ballR = Math.floor(Math.random() * 100);
 
  canvas.width = 480;
  canvas.height = 320;
@@ -12,6 +16,13 @@ ctx.fillstyle = 'black';
 ctx.beginPath();
 ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2 , true);
 ctx.fill();
+ if (counter >= 10) {
+
+ } else {
+ 	timeoutVar = setTimeout(start, 1000);
+ 	counter = counter + 1;
+  console.log("counter: " + counter);
+  }
 }
  return {
  	start: start
